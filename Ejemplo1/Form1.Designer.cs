dd3e150a5a2029e37497c742714bbb07a0ed42a4
David Tórtola <davidtortola_@hotmail.com>
2022-02-15 19:37:34 -0600

    Correccion a falta de nombre en boton
    Se le agrega el nombre al boton Guardar en el proyecto Ejemplo1 de ayd1

--- Ejemplo1/Form1.Designer.cs
+++ Ejemplo1/Form1.Designer.cs
@@ -44,11 +44,12 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(238, 96);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(240, 97);
+            this.button1.Size = new System.Drawing.Size(304, 142);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Guardar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
